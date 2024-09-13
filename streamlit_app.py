8b8121df2dfc47b5f1a76d43008695246c282729
Niklas Oeztekin <noeztek1@smail.uni-koeln.de>
2024-09-13 19:06:44 +0200

    first initialization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+
+st.set_page_config(
+    page_title="Climate Scenario Dashboard",
+    page_icon=":coffee:", 
+    layout="wide", 
+    initial_sidebar_state="auto", 
+    menu_items=None
+)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
